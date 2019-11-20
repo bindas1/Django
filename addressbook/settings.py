4ddb7af6cac4048b048e2bc71944d600f99e4bb5
Your Name <artq1406@gmail.com>
2019-11-20 20:33:29 +0100

    settings py adding static and media conf

--- addressbook/settings.py
+++ addressbook/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # internal apps
+    'contacts.apps.ContactsConfig'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR, 'static')
+}
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
